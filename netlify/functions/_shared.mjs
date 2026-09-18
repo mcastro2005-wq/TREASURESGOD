6aa9d17f4189ead01b0ba72376fd821e4d8d7445
mcastro2005-wq <mcastro2005@gmail.com>
2026-09-18 18:26:48 -0500

    Add files via upload

--- netlify/functions/_shared.mjs
+++ netlify/functions/_shared.mjs
@@ -3,7 +3,7 @@ import { getStore } from "@netlify/blobs";
 export function json(data, status = 200) {
   return new Response(JSON.stringify(data), {
     status,
-    headers: { "content-type": "application/json; charset=utf-8" },
+    headers: { "content-type": "application/json; charset=utf-8", "cache-control": "no-store" },
   });
 }
 
